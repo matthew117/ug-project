116e31dbbfac6629c160e5d8a03b5d3ffbd8a903
Matthew Bates <matthew117@hotmail.co.uk>
2012-11-18 23:09:05 +0000

    Created data structure for StaticObjects.
    Added some initial fields and methods to represent static objects
    within the map file.

--- src/uk/ac/dur/matthew/bates/ugproject/StaticObject.java
+++ src/uk/ac/dur/matthew/bates/ugproject/StaticObject.java
@@ -2,10 +2,35 @@ package uk.ac.dur.matthew.bates.ugproject;
 
 public class StaticObject
 {
+	private boolean castShadows;
+	private boolean collides;
+	private int fileIndex;
+	private int group;
+	private int id;
+	private String name;
+	private String tag;
+	private double[] rotation;
+	private double[] scale;
+	private double[] worldPos;
 
 	public StaticObject()
 	{
 		// TODO Auto-generated constructor stub
+	}
+
+	public double getMaxX()
+	{
+		return 0;
+	}
+
+	public double getMaxY()
+	{
+		return 0;
+	}
+
+	public double getMaxZ()
+	{
+		return 0;
 	}
 
 }
