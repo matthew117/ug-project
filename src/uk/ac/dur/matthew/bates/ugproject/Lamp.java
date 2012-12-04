1f81271d7ea17492ff996a840db074f576c43dd3
Matthew Bates <matthew117@hotmail.co.uk>
2012-12-04 21:26:01 +0000

    Added in support for light/lamp entities.
    This doesn't include primitive lighting objects.

---
+++ src/uk/ac/dur/matthew/bates/ugproject/Lamp.java
@@ -0,0 +1,22 @@
+package uk.ac.dur.matthew.bates.ugproject;
+
+public class Lamp extends Entity
+{
+	private boolean lit = true;
+	private String connectedLight;
+	private float connectionLightAmount = 1.0f;
+	private boolean connectionLightUseOnColor = true;
+	private boolean connectionLightUseSpec = false;
+	
+	protected String userVariablesDelegate()
+	{
+		String s = super.userVariablesDelegate();
+		s += "    <Var Name=\"Lit\" Value=\"" + lit + "\" />\n";
+		s += "    <Var Name=\"ConnectedLight\" Value=\"" + (connectedLight != null ? connectedLight : "") + "\" />\n";
+		s += "    <Var Name=\"ConnectionLightAmount\" Value=\"" + connectionLightAmount + "\" />\n";
+		s += "    <Var Name=\"ConnectionLightUserOnColor\" Value=\"" + connectionLightUseOnColor + "\" />\n";
+		s += "    <Var Name=\"ConnectionLightUseSpac\" Value=\"" + connectionLightUseSpec + "\" />\n";
+		return s;
+	}
+	
+}
