21bbc47431e33b75b995d79363bfc75d5a6058ce
Matthew Bates <matthew117@hotmail.co.uk>
2013-04-05 17:06:06 +0100

    Added stubs for DataFrames.

---
+++ src/uk/ac/dur/matthew/bates/ugproject/data/DataFrame.java
@@ -0,0 +1,11 @@
+package uk.ac.dur.matthew.bates.ugproject.data;
+
+import java.io.File;
+
+public class DataFrame
+{
+	public static DataFrame loadFromFile(File file)
+	{
+		return null;
+	}
+}
---
+++ test/uk/ac/dur/matthew/bates/ugproject/data/Test_DataFrame.java
@@ -0,0 +1,16 @@
+package uk.ac.dur.matthew.bates.ugproject.data;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Test_DataFrame
+{
+
+	@Test
+	public void test()
+	{
+		fail("Not yet implemented");
+	}
+
+}
