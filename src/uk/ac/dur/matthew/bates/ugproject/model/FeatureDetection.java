464a7c0bff3dd917ed5b1ef292e122dbf3f2b57e
Matthew Bates <matthew117@hotmail.co.uk>
2013-04-07 23:17:03 +0100

    Added stub functions and tests for floorplan feature detection.

---
+++ src/uk/ac/dur/matthew/bates/ugproject/model/FeatureDetection.java
@@ -0,0 +1,79 @@
+package uk.ac.dur.matthew.bates.ugproject.model;
+
+import java.util.List;
+
+public class FeatureDetection
+{
+	/**
+	 * Returns true if a is connected to b's top face
+	 */
+	public static boolean isDirectlyAbove(Rect a, Rect b)
+	{
+		return false;
+	}
+	
+	/**
+	 * Returns true if a is connected to b's bottom face
+	 */
+	public static boolean isDirectlyBelow(Rect a, Rect b)
+	{
+		return false;
+	}
+	
+	/**
+	 * Returns true if a is connected to b's left face
+	 */
+	public static boolean isDirectlyLeftOf(Rect a, Rect b)
+	{
+		return false;
+	}
+	
+	/**
+	 * Returns true is a is connected to b's right face
+	 */
+	public static boolean isDirectlyRightOf(Rect a, Rect b)
+	{
+		return false;
+	}
+	
+	/**
+	 * Returns true is a is connected to b's top-left corner
+	 */
+	public static boolean isOnTopLeftCorner(Rect a, Rect b)
+	{
+		return false;
+	}
+	
+	/**
+	 * Returns true is a is connected to b's top-right corner
+	 */
+	public static boolean isOnTopRightCorner(Rect a, Rect b)
+	{
+		return false;
+	}
+	
+	/**
+	 * Returns true is a is connected to b's bottom-left corner
+	 */
+	public static boolean isOnBottomLeftCorner(Rect a, Rect b)
+	{
+		return false;
+	}
+	
+	/**
+	 * Returns true is a is connected to b's bottom-right corner
+	 */
+	public static boolean isOnBottomRightCorner(Rect a, Rect b)
+	{
+		return false;
+	}
+	
+	/**
+	 * Returns a list of bottom-most rectangles
+	 */
+	public static List<Rect> listBottomMostRectangles(List<Rect> rects)
+	{
+		return null;
+	}
+	
+}
---
+++ test/uk/ac/dur/matthew/bates/ugproject/model/Test_FeatureDetection.java
@@ -0,0 +1,70 @@
+package uk.ac.dur.matthew.bates.ugproject.model;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class Test_FeatureDetection
+{
+
+	@Before
+	public void setUp() throws Exception
+	{
+	}
+
+	@Test
+	public void testIsDirectlyAbove()
+	{
+		fail("Test Not Implemented.");
+	}
+
+	@Test
+	public void testIsDirectlyBelow()
+	{
+		fail("Test Not Implemented.");
+	}
+
+	@Test
+	public void testIsDirectlyLeftOf()
+	{
+		fail("Test Not Implemented.");
+	}
+
+	@Test
+	public void testIsDirectlyRightOf()
+	{
+		fail("Test Not Implemented.");
+	}
+
+	@Test
+	public void testIsOnTopLeftCorner()
+	{
+		fail("Test Not Implemented.");
+	}
+
+	@Test
+	public void testIsOnTopRightCorner()
+	{
+		fail("Test Not Implemented.");
+	}
+
+	@Test
+	public void testIsOnBottomLeftCorner()
+	{
+		fail("Test Not Implemented.");
+	}
+
+	@Test
+	public void testIsOnBottomRightCorner()
+	{
+		fail("Test Not Implemented.");
+	}
+
+	@Test
+	public void testListBottomMostRectangles()
+	{
+		fail("Test Not Implemented.");
+	}
+
+}
